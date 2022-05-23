df84e9c7388b3eb1d5aac9821057d2b897cde3c2
Vansh1419 <vanshjain1419@gmail.com>
2022-05-23 23:59:57 +0530

    completed 8 components modified1

--- 50_Projects/8. Wave Form Animation/script.js
+++ 50_Projects/8. Wave Form Animation/script.js
@@ -3,18 +3,18 @@ console.log(inputs)
 inputs.forEach((input) => {
     input.addEventListener("click", (e) => {
         let count;
-        if (e.explicitOriginalTarget.classList.contains("inp1")) {
-            e.explicitOriginalTarget.nextElementSibling.classList.add("jump1")
-            if (e.explicitOriginalTarget.parentElement.nextElementSibling.children[0].value === "") {
-                e.explicitOriginalTarget.parentElement.nextElementSibling.children[1].classList.remove("jump2")
+        console.log(e);
+        if (e.target.classList.contains("inp1")) {
+            e.target.nextElementSibling.classList.add("jump1")
+            if (e.target.parentElement.nextElementSibling.children[0].value === "") {
+                e.target.parentElement.nextElementSibling.children[1].classList.remove("jump2")
             }
         }
-        else if (e.explicitOriginalTarget.classList.contains("inp2")) {
-            e.explicitOriginalTarget.nextElementSibling.classList.add("jump2")
-            if (e.explicitOriginalTarget.parentElement.previousElementSibling.children[0].value === "") {
-                e.explicitOriginalTarget.parentElement.previousElementSibling.children[1].classList.remove("jump1")
+        else if (e.target.classList.contains("inp2")) {
+            e.target.nextElementSibling.classList.add("jump2")
+            if (e.target.parentElement.previousElementSibling.children[0].value === "") {
+                e.target.parentElement.previousElementSibling.children[1].classList.remove("jump1")
             }
         }
-        console.log(e.explicitOriginalTarget.parentElement.nextElementSibling.children[0]);
     })
 })
